bbaa592390413a5acbdd2c4936e6bc7c04243b97
Joshua Britain <50422789+jbritain@users.noreply.github.com>
2023-09-24 14:44:05 +0100

    integrate twitch

--- aberlink/src/components/VideoStream.js
+++ aberlink/src/components/VideoStream.js
@@ -1,14 +1,10 @@
 import React, { useState, useCallback } from 'react';
 import './VideoStream.css'
+import { TwitchPlayerNonInteractive } from 'react-twitch-embed';
 
-const VideoStream = ({embedId}) => {
+const VideoStream = ({channelID}) => {
     return (
-        <div className="video-background">
-        <iframe
-            src="https://www.youtube.com/embed/E9iP8jdtYZ0?si=2L-zOGJpApC9dCv5&autoplay=1&mute=1"
-            allow="autoplay"
-       />
-        </div>
+        <TwitchPlayerNonInteractive className='video' channel={channelID} autoplay muted />
     )
 }
 
