74d9fe83862daf623b3e74e1b741957fa1d3cb6f
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2023-09-24 14:43:45 +0100

    edited visit page

--- aberlink/src/components/UI/Instructions.js
+++ aberlink/src/components/UI/Instructions.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './Interface.css';
 const Instructions = props => {
 
+    
+
     return (
         <div className="text">
-            <p>This Counter shows how many people have been to both sides of this portal.</p>
-            <p>After you scan the qr code down below, head over to location{props.loc} to scan the second qr code and increase the counter! </p>
+            <p>This Counter shows how many people have scanned the qr codes on both sides of this portal.</p>
+            <p>After you scan the qr code down below, head over to location {props.loc === 1 ? 2 : 1} to scan the second qr code if you have not done so already! </p>
         </div>
     )
 }
